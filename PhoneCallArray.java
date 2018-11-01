58774ec0242b203bed4def30e481ebcecc6f3623
luckdiller <44411349+luckdiller@users.noreply.github.com>
2018-11-01 12:56:04 -0500

    Add files via upload

--- PhoneCallArray.java
+++ PhoneCallArray.java
@@ -1,13 +1,14 @@
 /*
- * Name:        James Horton
- * Date:        11/01/2018
- * File:        PhoneCallArray.java
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
  */
-package phonecallarray;
-
+package phonecall;
+import java.util.ArrayList;
+import java.util.Scanner;
 /**
  *
- * @author jh0375800
+ * @author lm0865474
  */
 public class PhoneCallArray 
 {
@@ -17,6 +18,42 @@ public class PhoneCallArray
      */
     public static void main(String[] args) 
     {
+        ArrayList<String> call = new ArrayList<String>();
+        
+        String flag ="";
+        Scanner key = new Scanner(System.in);
+        while(!"Q".equals(flag)){
+            
+            System.out.println("Was the call incoming? (Y or N or Q to quit): ");
+            String inp = key.next();
+            if ("Y".equals(inp)){
+                System.out.println("Enter Phone Number: ");
+                String num = key.next();
+                IncomingPhoneCall inc = new IncomingPhoneCall(num);
+                inc(num);
+                
+                call.add(num);   
+            }
+            if("N".equals(inp)){
+                System.out.println("Enter Phone Number: ");
+                String num = key.next();
+                System.out.println("How Long was the Call: ");
+                String leng = key.next();
+                Double conv = Double.parseDouble(leng);
+                OutgoingPhoneCall ouc = new OutgoingPhoneCall(num,conv);
+                
+                call.add(num);
+                call.add(leng);
+            }
+          
+            
+        }
+        
+//        System.out.println(num);
+//        System.out.println(leng);
+//        System.out.println(inp);
+        
+        
         
         
     } // end of main
